b768b9d5698afd2ca5c908e0c41f7cdf26867f7b
NADESHIKON <allenzhang040817@gmail.com>
2022-07-26 20:30:21 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RedocStandalone } from 'redoc';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RedocStandalone specUrl="https://raw.githubusercontent.com/Enime-Project/api.enime.moe/dev/api-definition.json" options={{
+        hideDownloadButton: true,
+        disableSearch: false,
+        pathInMiddlePanel: false,
+        menuToggle: true,
+        hideSchemaTitles: false,
+        theme: {
+          colors: {
+            border: {
+              light: "#e20069",
+              dark: "#afac00"
+            },
+            primary: {
+              main: "#e20069",
+              dark: "#afac00"
+            },
+            text: {
+              primary: "#000",
+              secondary: "#000"
+            }
+          },
+          logo: {
+            maxHeight: "150px",
+            maxWidth: "150px",
+          },
+          rightPanel: {
+            backgroundColor: "rgb(12, 0, 39)",
+            servers: {
+              overlay: {
+                backgroundColor: "#e20069",
+                textColor: "#000"
+              },
+            }
+          },
+          typography: {
+            fontSize: "17px"
+          }
+        }
+      }} />
   );
 }
 
